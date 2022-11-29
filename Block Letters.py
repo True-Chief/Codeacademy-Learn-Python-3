823de90383884b0a3e497fb963c6c9b51571f647
Ethan Jones <118764271+True-Chief@users.noreply.github.com>
2022-11-29 22:59:57 +0000

    Block Letters

---
+++ Block Letters.py
@@ -0,0 +1,10 @@
+#Ethan Jones
+#I've played Ice Hockey for Cardiff
+
+print("EEEEE      JJJJJ")
+print("E            J")
+print("E            J")
+print("EEEEE        J")
+print("E         J  J")
+print("E         J  J")
+print("EEEEE       J")
